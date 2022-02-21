559e2979db757af92474dfd80e1d6ba3147b6399
nur-ipek <nur.ipek.yilmaz@gmail.com>
2022-02-21 14:42:38 +0300

    Add entity classes.

---
+++ AuthServerPractice.Core/Models/UserApp.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServerPractice.Core.Models
+{
+    public class UserApp: IdentityUser //Identıty user içerisinde bulunan alanlar veri tabanına sutün olarak eklenmektedir.  
+    {
+        public string City { get; set; }
+
+    }
+}

56435a5f4bd606d42087f31706e028e1c707d1ff
nur-ipek <nur.ipek.yilmaz@gmail.com>
2022-02-23 11:43:59 +0300

    Add IGenericRepository class.

---
+++ AuthServerPractice.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServerPractice.Core.Repositories
+{
+    public interface IGenericRepository<TEntity> where TEntity: class
+    {
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<TEntity> GetByIdAsyns(int id);
+        Task AddAsync(TEntity entity);
+        void Remove(TEntity entity);
+        void Update(TEntity entity);
+        Task<IQueryable<TEntity>> Where(Expression<Func<TEntity, bool>> predi);
+
+    }
+}

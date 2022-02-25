2fe85c30af68352bbdedec20f1383870375f3988
nur-ipek <nur.ipek.yilmaz@gmail.com>
2022-02-25 16:56:39 +0300

    Add TokenDto.cs and ClientTokenDto.cs

---
+++ AuthServerPractice.Core/DTOs/ClientTokenDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServerPractice.Core.DTOs
+{
+    //Client'lara döneceğimiz token modelleri
+    //Hava tahmini, yemek tarifi dönen üyelik sistemi gerektirmeyen API'ler için Client'ın kendisini tanımlayan token gereklidir.
+    public class ClientTokenDto
+    {
+        public string AccessToken { get; set; }
+        public DateTime AccessTokenExpiration { get; set; }
+    }
+}
---
+++ AuthServerPractice.Core/DTOs/TokenDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServerPractice.Core.DTOs
+{
+    //Client'lara döneceğimiz token modelleri
+    public class TokenDto
+    {
+        public string AccessToken { get; set; }
+        public DateTime AccessTokenExpiration { get; set; }
+        public string RefreshToken { get; set; }
+        public DateTime RefreshTokenExpiration { get; set; }
+    }
+}

019d0786c57b3ea4dec87cb878846274088b9bbc
nur-ipek <nur.ipek.yilmaz@gmail.com>
2022-02-26 09:58:15 +0300

    Edit ITokenService

--- AuthServerPractice.Core/Services/ITokenService.cs
+++ AuthServerPractice.Core/Services/ITokenService.cs
@@ -12,6 +12,6 @@ namespace AuthServerPractice.Core.Services
     {
         TokenDto CreateToken(UserApp userApp);
 
-        ClientTokenDto CreateTokenByClient(ClientTokenDto client)
+        ClientTokenDto CreateTokenByClient(ClientTokenDto client);
     }
 }

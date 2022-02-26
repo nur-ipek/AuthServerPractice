e2261c436eaab7068e8065f87945a84de10731bb
nur-ipek <nur.ipek.yilmaz@gmail.com>
2022-02-26 09:55:15 +0300

    Add ITokenService and Client.cs

---
+++ AuthServerPractice.Core/Configurations/Client.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServerPractice.Core.Configurations
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Secret { get; set; }
+
+        //PAYLOAD bölümünde görünecek..
+        public List<String> Audiences { get; set; }
+    }
+}
---
+++ AuthServerPractice.Core/Services/ITokenService.cs
@@ -0,0 +1,17 @@
+﻿using AuthServerPractice.Core.DTOs;
+using AuthServerPractice.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServerPractice.Core.Services
+{
+    public interface ITokenService
+    {
+        TokenDto CreateToken(UserApp userApp);
+
+        ClientTokenDto CreateTokenByClient(ClientTokenDto client)
+    }
+}

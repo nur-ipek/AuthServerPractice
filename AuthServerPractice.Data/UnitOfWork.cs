270c87731181b8d8f76a6df58968d6d3eb53717d
nur-ipek <nur.ipek.yilmaz@gmail.com>
2022-03-06 14:27:32 +0300

    Add UnitOfWork.cs

---
+++ AuthServerPractice.Data/UnitOfWork.cs
@@ -0,0 +1,32 @@
+﻿using AuthServerPractice.Core.UnitOfWork;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServerPractice.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        //SaveChange çağırabilmem için DbCpontext'e ihyicımız var.
+        //temel context
+        private readonly DbContext _dbContext;
+
+        //Burada AppDbCopntext'i DI nesnesi olarak geçiyoruz.
+        public UnitOfWork(AppDbContext appDbContext)
+        {
+            _dbContext = appDbContext;
+        }
+        public void Commit()
+        {
+            _dbContext.SaveChanges();
+        }
+
+        public async Task CommitAsync()
+        {
+           await _dbContext.SaveChangesAsync();
+        }
+    }
+}

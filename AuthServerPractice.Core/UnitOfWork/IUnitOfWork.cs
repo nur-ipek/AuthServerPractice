d71b326f64bc80f6c6cd6524e9248c54509265cf
nur-ipek <nur.ipek.yilmaz@gmail.com>
2022-03-06 11:53:55 +0300

    Add IUnitOfWork

---
+++ AuthServerPractice.Core/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServerPractice.Core.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        //Task Asenkron'da void'e karşılık gelir.
+        Task CommitAsync();
+        void Commit();
+    }
+}

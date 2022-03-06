d4bb7851e2df86bda27de98f836418db20bd15fd
nur-ipek <nur.ipek.yilmaz@gmail.com>
2022-03-06 12:25:52 +0300

    Add AppDbContext.cs and install required package

---
+++ AuthServerPractice.Data/AppDbContext.cs
@@ -0,0 +1,32 @@
+﻿using AuthServerPractice.Core.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServerPractice.Data
+{
+    //Üyelik sistemi ile ilgili de bir context olduğundan dolayı IdentityDbContext kullanıyoruz.
+    //IdentiyyRole --> Identity kütüphanesinden geliyor.
+    public class AppDbContext:IdentityDbContext<UserApp,IdentityRole,string>
+    {
+        //DbContextOptions'ı startup.s tarafında dolduruluyoruzzz !11!
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        //Kullanıcı ile ilgili tüm DbSet'ler IdentityDbContext'ten miras olarak geliyorrr !11! (Token, Roller tutan tablolar vb.)
+        public DbSet<Product> Products { get; set; }
+        public DbSet<UserRefreshToken> UserRefreshToken { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}

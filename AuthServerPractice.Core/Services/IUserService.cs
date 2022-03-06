7d8be80b44c633ebc0cd19f9afd3e568e9e01ac4
nur-ipek <nur.ipek.yilmaz@gmail.com>
2022-03-06 11:46:39 +0300

    Add IUserService, CreateUserDto and UserAppDto

---
+++ AuthServerPractice.Core/Services/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using AuthServerPractice.Core.DTOs;
+using SharedLibrary.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServerPractice.Core.Services
+{
+    public interface IUserService
+    {
+        Task<Response<UserAppDto>> CreateUserAsync(CreateUserDto createUserDto);
+
+
+    }
+}
